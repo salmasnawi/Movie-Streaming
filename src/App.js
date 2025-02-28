b10f6e522f988d4e274bc29e6ee8ad18cf6c90ab
salma <salmaatef272@gmaile.com>
2025-02-28 20:30:08 +0200

    lastchange

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home/*" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<Signup />} />
        
         <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
