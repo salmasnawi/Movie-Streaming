5149e4aa36a4ac245b8fbae4f75fd7a87dfdb8f8
salma <salmaatef272@gmaile.com>
2025-02-28 20:40:17 +0200

    lastcommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./pages/Home";
@@ -9,22 +9,19 @@ import "./App.css";
 import MyNavbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-
 const App = () => {
   return (
     <Router>
       <MyNavbar />
       <Routes>
-      <Route path="*" element={<Navigate to="/" />} /> {/* إعادة التوجيه إلى Home عند مسار غير معروف */}
-
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home/*" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-       
         <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="*" element={<Navigate to="/home" />} /> {/* توجيه المسارات غير المعروفة إلى /home */}
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 };
